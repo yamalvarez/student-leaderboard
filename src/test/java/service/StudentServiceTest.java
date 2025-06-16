a74190faf35d3e18333e55e843addf25c1c37ffa
Yasiel <yamalvarez89@gmail.com>
2025-06-16 16:08:39 -0400

    Add unit tests for StudentService using Mockito

--- src/test/java/service/StudentServiceTest.java
+++ src/test/java/service/StudentServiceTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import service.StudentService;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 public class StudentServiceTest {
 
