33777360fc096c3d2a38947d2fe823739965666f
Yasiel <yamalvarez89@gmail.com>
2025-06-19 22:38:39 -0400

    Add Swagger Dependency

---
+++ src/main/java/com/yasiel/studentleaderboard/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.yasiel.studentleaderboard.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Student Leaderboard API")
+                        .version("1.0")
+                        .description("API to manage and rank students by score"));
+    }
+}
